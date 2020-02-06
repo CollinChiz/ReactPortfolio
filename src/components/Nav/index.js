2d99c2bc799637bd898dd9ff6511e71914afbc03
chizzy <cwchiz16@gmail.com>
2020-02-05 19:30:27 -0700

    fixed nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,33 +4,19 @@ import "./style.css"
 export default function Nav() {
 
   const style = {
-    margin: 0,
-    top: 0,
-    right: 60,
-    bottom:20,
-    left: 'auto',
-    position: 'fixed',
+    
   }
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">
-        Collin Chisholm
-      </a>
-      <div id="navbarSupportedContent">
-        <ul style={style} className="nav navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="/about">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            Collin Chisholm
+          </a>
+          <a className="nav-link" href="/about">
               About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/portfolio">
+          </a>
+          <a className="nav-link" href="/portfolio">
                 Portfolio
-            </a>
-          </li>
-
-        </ul>
-      </div>
-    </nav>
+          </a>
+      </nav>
     )
 }
